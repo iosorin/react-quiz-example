3f26ca758140bfdc65d8c796a4cf7b3d77b2a21e
osorina <official.osorina@gmail.com>
2020-10-07 22:45:50 +0200

    action creators, action types

--- src/redux/reducers/clicked.js
+++ src/redux/reducers/clicked.js
@@ -1,10 +1,12 @@
+import { UPDATE_CLICKED } from '../actions/actionTypes';
+
 const initialState = {
     clicked: false
 };
 
 export default function rootReducer(state = initialState, action) {
     switch (action.type) {
-        case 'UPDATE_CLICKED':
+        case UPDATE_CLICKED:
             return {
                 clicked: !state.clicked
             };
