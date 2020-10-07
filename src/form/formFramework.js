270b30285d9c7cf1bc90b88c0989fb736c102354
iosorin <miroslav.osorin@gmail.com>
2020-10-07 15:29:12 +0200

    Finish front -> goto Firebase integration

---
+++ src/form/formFramework.js
@@ -0,0 +1,39 @@
+export function createControl(config, validation) {
+    return {
+        ...config,
+        validation,
+        valid: !validation,
+        touched: false,
+        value: ''
+    };
+}
+
+export function validate(value, validation = null) {
+    if (!validation) return true;
+
+    let isValid = true;
+
+    if (validation.required) {
+        isValid = value.trim() !== '' && isValid;
+    }
+
+    if (validation.email) {
+        isValid = /^\S+@\S+\.\S{2,3}$/.test(value) && isValid;
+    }
+
+    if (validation.minLength) {
+        isValid = value.length >= validation.minLength && isValid;
+    }
+
+    return isValid;
+}
+
+export function validateForm(formControls) {
+    let isFormValid = true;
+
+    Object.keys(formControls).forEach((controlName) => {
+        isFormValid = formControls[controlName].valid && isFormValid;
+    });
+
+    return isFormValid;
+}
