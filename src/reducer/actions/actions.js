bb698827781300e7a24d4104e4c6661cab242a11
iosorin <miroslav.osorin@gmail.com>
2020-10-08 00:13:18 +0200

    fetch quiz by id (p/1)

--- src/reducer/actions/actions.js
+++ src/reducer/actions/actions.js
@@ -1,4 +1,9 @@
-import { FETCH_QUIZES_ERROR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS } from './actionTypes';
+import {
+    FETCH_QUIZES_ERROR,
+    FETCH_QUIZES_START,
+    FETCH_QUIZES_SUCCESS,
+    FETCH_QUIZ_SUCCESS
+} from './actionTypes';
 import axiosQuiz from '../../axios';
 
 export function fetchQuizes(payload) {
@@ -22,17 +27,44 @@ export function fetchQuizes(payload) {
     };
 }
 
+export function fetchQuizById(quizId) {
+    return async (dispatch) => {
+        dispatch(fetchQuizesStart());
+
+        try {
+            const response = await axiosQuiz.get('/quizes/' + quizId + '.json');
+            const quiz = response;
+
+            dispatch(fetchQuizSuccess(quiz.data));
+
+            // this.setState({ quiz, loading: false });
+        } catch (error) {
+            fetchQuizesError(error);
+            console.log(error);
+        }
+    };
+}
+
 export function fetchQuizesStart() {
     return {
         type: FETCH_QUIZES_START
     };
 }
+
+export function fetchQuizSuccess(quiz) {
+    return {
+        type: FETCH_QUIZ_SUCCESS,
+        quiz
+    };
+}
+
 export function fetchQuizesSuccess(quizes) {
     return {
         type: FETCH_QUIZES_SUCCESS,
         quizes
     };
 }
+
 export function fetchQuizesError(error) {
     return {
         type: FETCH_QUIZES_ERROR,
--- src/reducer/reducers/quiz.js
+++ src/reducer/reducers/quiz.js
@@ -1,16 +1,23 @@
 import {
     FETCH_QUIZES_ERROR,
     FETCH_QUIZES_START,
-    FETCH_QUIZES_SUCCESS
+    FETCH_QUIZES_SUCCESS,
+    FETCH_QUIZ_SUCCESS
 } from '../actions/actionTypes';
 
 const initialState = {
     quizes: [],
     loading: false,
-    error: null
+    error: null,
+    results: {}, // {[id]: 'success' 'error'}
+    isFinished: false,
+    activeQuestion: 0,
+    answerState: null, // {[id]: 'success' 'error'}
+    quiz: []
 };
 
 export default function quizReducer(state = initialState, action) {
+    console.log('action', action);
     switch (action.type) {
         case FETCH_QUIZES_START:
             return {
@@ -23,6 +30,14 @@ export default function quizReducer(state = initialState, action) {
                 quizes: action.quizes,
                 loading: false
             };
+
+        case FETCH_QUIZ_SUCCESS:
+            return {
+                ...state,
+                quiz: action.quiz,
+                loading: false
+            };
+
         case FETCH_QUIZES_ERROR:
             return {
                 ...state,
