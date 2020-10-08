2a576c59b32c8f6d0f1859b5bf1ca12fca77ce54
osorina <official.osorina@gmail.com>
2020-10-08 10:10:33 +0200

    package.json fix

---
+++ src/App.test.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import { configure, shallow } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+
+// import App from './App';
+// import QuizList from './containers/QuizList/QuizList';
+
+// configure({
+//     adapter: Adapter
+// });
+
+// describe('<App />', () => {
+//     it('Should render quiz list', () => {
+//         const wrapper = shallow(<App />);
+
+//         expect(wrapper.find(QuizList)).toHaveLength(1);
+//     });
+// });
