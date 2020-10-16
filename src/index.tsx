6e6d411930154e4af9f1eea90237996cb63d55c2
iosorin <miroslav.osorin@gmail.com>
2020-10-16 21:06:56 +0200

    check

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import rootReducer from './reducer/reducers/rootReducer';
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+const application = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render(application, document.getElementById('root'));
+
+serviceWorker.unregister();
