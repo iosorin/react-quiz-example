c5a18d0d77e1436875753ea23468dc85fd7a55b5
iosorin <miroslav.osorin@gmail.com>
2020-10-18 04:35:04 +0200

    savepoint

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
-export default axios.create({
+const config = {
     baseURL: 'https://react-quiz-8d50b.firebaseio.com/',
-});
+};
+
+/* todo: fix any */
+const api: any = axios.create(config);
+
+export default api;
